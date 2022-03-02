a1bd3f5ecd191b50de4f6bcd8008e2ecff7abf01
4rutemu <artyom.denega@gmail.com>
2022-03-02 13:45:11 +0300

    Задания 5.2 и 5.4

--- app/src/androidTest/java/com/denega/practice1/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/denega/practice1/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.denega.practice1", appContext.packageName)
+        assertEquals("com.denega.app", appContext.packageName)
     }
 }
